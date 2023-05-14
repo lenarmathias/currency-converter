cc7cc0b5e8a360e7f0720d9b7b8d1d50cb226c0c
Maciej Lenartowicz <lenarmathias@gmail.com>
2023-05-14 12:35:22 +0200

    assigned addEventListener's to init()

--- js/script.js
+++ js/script.js
@@ -4,40 +4,36 @@
     };
 
     const outputResult = (inputValue, outputValue, currencySelector) => {
-        inputValue.addEventListener("input", () => {
-            const ratePlnEur = 4.52;
-            const ratePlnUsd = 4.11;
-            let convertedValue;
-            let currency;
+        const ratePlnEur = 4.52;
+        const ratePlnUsd = 4.11;
+        let convertedValue;
+        let currency;
 
-            switch (currencySelector.value) {
-                case ("plnToEur"):
-                    convertedValue = inputValue.value / ratePlnEur;
-                    currency = "EUR";
-                    break;
-                case ("eurToPln"):
-                    convertedValue = inputValue.value * ratePlnEur;
-                    currency = "PLN";
-                    break;
-                case ("plnToUsd"):
-                    convertedValue = inputValue.value / ratePlnUsd;
-                    currency = "USD";
-                    break;
-                case ("usdToPln"):
-                    convertedValue = inputValue.value * ratePlnUsd;
-                    currency = "PLN";
-                    break;
-            };
+        switch (currencySelector.value) {
+            case ("plnToEur"):
+                convertedValue = inputValue.value / ratePlnEur;
+                currency = "EUR";
+                break;
+            case ("eurToPln"):
+                convertedValue = inputValue.value * ratePlnEur;
+                currency = "PLN";
+                break;
+            case ("plnToUsd"):
+                convertedValue = inputValue.value / ratePlnUsd;
+                currency = "USD";
+                break;
+            case ("usdToPln"):
+                convertedValue = inputValue.value * ratePlnUsd;
+                currency = "PLN";
+                break;
+        };
 
-            outputValue.innerText = `Kwota po przeliczeniu: ${(convertedValue).toFixed(2)} ${currency}`;
-        });
+        outputValue.innerText = `Kwota po przeliczeniu: ${(convertedValue).toFixed(2)} ${currency}`;
     };
 
     const selectorReset = (inputValue, outputValue, currencySelector) => {
-        currencySelector.addEventListener("change", () => {
-            inputValue.value = "";
-            outputValue.innerText = "";
-        });
+        inputValue.value = "";
+        outputValue.innerText = "";
     };
 
     function init() {
@@ -46,8 +42,8 @@
         const currencySelector = document.querySelector(".js-currencySelector");
 
         welcome();
-        outputResult(inputValue, outputValue, currencySelector);
-        selectorReset(inputValue, outputValue, currencySelector);
+        inputValue.addEventListener("input", () => outputResult(inputValue, outputValue, currencySelector));
+        currencySelector.addEventListener("change", () => selectorReset(inputValue, outputValue, currencySelector));
     };
 
     init();
